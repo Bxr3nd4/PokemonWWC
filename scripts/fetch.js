03039e35f8422055cb963001bbaa1140b250bda6
Bxr3nd4 <Fiona5109@hotmail.com>
2023-06-02 18:30:53 -0500

    Update

---
+++ scripts/fetch.js
@@ -0,0 +1,25 @@
+// const url = 'https://pokeapi.co/api/v2/pokemon'
+
+// const character = fetch(url)
+//     .then(res => res.json())
+//     .then(data => {
+//         data.results.forEach(character => {
+//             console.log(character.name)
+//         });
+//     })
+
+
+let url = "https://pokeapi.co/api/v2/pokemon";
+
+const getPokemons = async () => {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data.results;
+  } catch (error) {
+    console.log(error);
+  }
+};
+const pokemons = getPokemons().then((data) => {
+  console.log(data);
+});
