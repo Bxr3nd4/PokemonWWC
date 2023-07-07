93b537e6f05b5c22fed6279833b763e140b97f0c
Bxr3nd4 <Fiona5109@hotmail.com>
2023-07-06 20:23:50 -0500

    Adding JS

---
+++ scripts/buycard.js
@@ -0,0 +1,69 @@
+
+
+let url = "https://pokeapi.co/api/v2/";
+
+const getpokemon = async() => {
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        data.results.forEach(async(pokemon) => {
+            const respons = await fetch(pokemon.url);
+            const dataPokemon = respons.json();
+
+            const [type1,type2]= dataPokemon.types.map(
+                (typePokemon) => typePokemon.type.name
+            );
+
+            const container = document.querySelector('.container');
+
+            let pokeCard = document.createEelemnt('div');
+            pokeCard.className = 'pokeCard';
+            pokeCard.innerHTML = `
+            <div class = "headerCard">
+                <p>${dataPokemon.name}</p>
+                <i class = "fa-sharp fa-regular fa-heart"></i>
+            </div>
+
+            <img class = "imgPoke" src = "${dataPokemon.sprites.other["home"].front_default}">
+            <div>
+                <p>Exp ${dataPokemon.base_experience}</p>
+                <button>Buy</button>
+            </div>
+
+        `
+                container.appendChild(pokeCard);
+
+                pokeCard.setAttribute("type1",type1);
+                pokeCard.setAttribute("type2", type2);
+            
+        });
+    }catch (error){
+        alert("Error");
+    }
+}
+
+getpokemon();
+
+const filter = document.querySelectorAll('.type');
+
+filter.forEach((filterType) => {
+    filterType.addEventListener("click", (event) => {
+      event.preventDefault();
+      const type = filterType.textContent.toLowerCase();
+      filterByType(type);
+    });
+  });
+
+  const filterByType = (type) => {
+    const cards = document.querySelectorAll(".pokeCard");
+    cards.forEach((card) => {
+      const cardType1 = card.getAttribute("type1");
+      const cardType2 = card.getAttribute("type2");
+  
+      if (type === "all" || cardType1 === type || cardType2 === type) {
+        card.classList.remove("hidden");
+      } else {
+        card.classList.add("hidden");
+      }
+    });
+  };
