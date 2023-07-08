be745f0846d4bcfc3cbfacc49ea6391333480089
Bxr3nd4 <Fiona5109@hotmail.com>
2023-07-07 22:49:47 -0500

    fix

--- scripts/buycard.js
+++ scripts/buycard.js
@@ -23,7 +23,7 @@ const getpokemon = async() => {
             </div>
 
             <img class = "imgPoke" src = "${dataPokemon.sprites.other["home"].front_default}">
-            <div>
+            <div class="buybtn">
                 <p>Exp ${dataPokemon.base_experience}</p>
                 <button>Buy</button>
             </div>
