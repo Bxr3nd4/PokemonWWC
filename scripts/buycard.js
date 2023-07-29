73bdea5c9e20244d30a093378f5b9dac929b4a26
Bxr3nd4 <Fiona5109@hotmail.com>
2023-07-29 13:46:27 -0500

    fixing errors

--- scripts/buycard.js
+++ scripts/buycard.js
@@ -1,22 +1,32 @@
 let url = "https://pokeapi.co/api/v2/pokemon";
+let offset=0;
 
-const getpokemon = async() => {
-    try {
-        const res = await fetch(url);
-        const data = await res.json();
-        data.results.forEach(async(pokemon) => {
-            const respons = await fetch(pokemon.url);
-            const dataPokemon = await respons.json();
 
-            const [type1,type2]= dataPokemon.types.map(
-                (typePokemon) => typePokemon.type.name
-            );
+const getpokemon = async () => {
+  let totalCards = document.querySelector(".showcard");
+  let showMore = document.querySelector(".showmore");
 
-            const container = document.querySelector('.container');
+  const res = await fetch(`${url}?offset=${offset}`);
+  // console.log(res);
+  // try {
+  const data = await res.json();
+  let nextLink;
+  console.log(data.next)
+  if (!res.ok) throw { status: res.status, statusText: res.statusText }
+  data.results.forEach(async (pokemon) => {
+    const respons = await fetch(pokemon.url);
+    const dataPokemon = await respons.json();
+    // console.log(respons)
 
-            let pokeCard = document.createElement('div');
-            pokeCard.className = 'pokeCard';
-            pokeCard.innerHTML = `
+    const [type1, type2] = dataPokemon.types.map(
+      (typePokemon) => typePokemon.type.name
+    );
+
+    const container = document.querySelector('.container');
+
+    let pokeCard = document.createElement('div');
+    pokeCard.className = 'pokeCard';
+    pokeCard.innerHTML = `
             <div class = "headerCard">
                 <p>${dataPokemon.name}</p>
                 <i class = "fa-sharp fa-regular fa-heart"></i>
@@ -25,43 +35,62 @@ const getpokemon = async() => {
             <img class = "imgPoke" src = "${dataPokemon.sprites.other["home"].front_default}">
             <div class="buybtn">
                 <p class="xp" >Exp ${dataPokemon.base_experience}</p>
-                <button>Buy</button>
+                <button class="bcards">Buy</button>
             </div>
 
         `
-                container.appendChild(pokeCard);
+    container.appendChild(pokeCard);
 
-                pokeCard.setAttribute("type1",type1);
-                pokeCard.setAttribute("type2", type2);
-            
-        });
-    }catch (error){
-        alert("Error, checkea el codigo :D");
-    }
+    pokeCard.setAttribute("type1", type1);
+    pokeCard.setAttribute("type2", type2);
+
+    let headerCard = document.querySelectorAll(".pokeCard");
+    console.log(headerCard.length);
+    console.log(data);
+    console.log(data.next)
+    nextLink = data.next ? `<a class="showcard" href="${data.next}"> Show more cards </a>` : "";
+
+    showMore.innerHTML = nextLink;
+  });
+
+  console.log(data.next)
+
+  //     }catch (error){
+  //         console.log(error);
+  //     }
 }
 
 getpokemon();
 
+document.addEventListener("click", (e) => {
+  if (e.target.matches(".showmore a")) {
+    e.preventDefault();
+    offset += 20;
+    getpokemon(e.target.getAttribute("href"));
+  }
+}
+)
+
 const filter = document.querySelectorAll('.type');
 
 filter.forEach((filterType) => {
-    filterType.addEventListener("click", (event) => {
-      event.preventDefault();
-      const type = filterType.textContent.toLowerCase();
-      filterByType(type);
-    });
+  filterType.addEventListener("click", (event) => {
+    event.preventDefault();
+    const type = filterType.textContent.toLowerCase();
+    filterByType(type);
   });
+});
 
-  const filterByType = (type) => {
-    const cards = document.querySelectorAll(".pokeCard");
-    cards.forEach((card) => {
-      const cardType1 = card.getAttribute("type1");
-      const cardType2 = card.getAttribute("type2");
-  
-      if (type === "all" || cardType1 === type || cardType2 === type) {
-        card.classList.remove("hidden");
-      } else {
-        card.classList.add("hidden");
-      }
-    });
-  };
+const filterByType = (type) => {
+  const cards = document.querySelectorAll(".pokeCard");
+  cards.forEach((card) => {
+    const cardType1 = card.getAttribute("type1");
+    const cardType2 = card.getAttribute("type2");
+
+    if (type === "all" || cardType1 === type || cardType2 === type) {
+      card.classList.remove("hidden");
+    } else {
+      card.classList.add("hidden");
+    }
+  });
+};
